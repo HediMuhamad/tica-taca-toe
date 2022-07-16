758808680a88183a137f790f78fac32a3b861ae2
HediMuhamad <hedymuhamad461@gmail.com>
2022-07-17 01:30:28 +0300

    Customized template to the project, now it's ready to Air-Strike. :D
    assets added, fonts and it's styles, color-plate added, sass added and all css converted to scss.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/font-styles.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
